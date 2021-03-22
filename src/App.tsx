b951ef52f958b43b9aff32f8a38a3beab46d3045
BePoyerGabe <bernardopoyer00@gmail.com>
2021-03-22 12:00:30 -0300

    miragejs and react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionModal } from "./components/TransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen}/>
       <Dashboard />
+
+      <TransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModalOpen}/>
+
+
       <GlobalStyle />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transaction', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 420,
+          type: 'deposit',
+          category: 'food',
+          createdAt: '12/10/2019'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
